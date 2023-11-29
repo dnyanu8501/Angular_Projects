f3a51ea177f6ab51b390039d1d2f0e561a90d7d5
dnyanu8501 <dnyaneshwargawade79@gmail.com>
2023-11-29 13:12:59 +0530

    done

--- Hotel_Management/src/app/owner/hotel-register/hotel-register.component.ts
+++ Hotel_Management/src/app/owner/hotel-register/hotel-register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiCallService } from 'src/app/commonSevices/api-call.service';
 
 @Component({
   selector: 'app-hotel-register',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./hotel-register.component.scss']
 })
 export class HotelRegisterComponent {
+  registerForm!:FormGroup;
+  endPoint="hotelDetails"
+
+constructor(private router:Router,private fb:FormBuilder,private apiCallService:ApiCallService){}
+ngOnInit(){
+  this.formAllData()
+}
+formAllData(){
+  this.registerForm=this.fb.group({
+    ownername:['',[Validators.required]],
+    ownermobile:[],
+    hotelname:[],
+    hoteladdress:[],
+    hotelmobile:[],
+    rating:[],
+    room:[],
+    price:[],
+    hotelimages:[]
+
+  })
+}
+
+submit(){
+  this.apiCallService.postApiCall(this.endPoint,this.registerForm.value).subscribe(res =>{
+    this.router.navigateByUrl("owner/ownersuccess")
+  })
+}
+  back(){
+    this.router.navigateByUrl("owner/ownersuccess")
+  }
 
 }
